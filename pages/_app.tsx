4e0eae36614d687f7d28f60ef5a75917565d6a68
Louis Orleans <louis@orleans.io>
2020-09-28 12:30:17 -0700

    ✨ use sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: { Component: any; pageProps: any[] }) {
   return <Component {...pageProps} />;
